c3d89ada24991e6f2ee9061129b81be9986a846e
Barry Plunkett <eplu@sas.upenn.edu>
2018-01-17 13:50:10 -0500

    part 1 and complexity baseline started

---
+++ barry_hw2_skeleton.py
@@ -0,0 +1,148 @@
+#############################################################
+## ASSIGNMENT 2 CODE SKELETON
+## RELEASED: 1/17/2018
+## DUE: 1/24/2018
+## DESCRIPTION: In this assignment, you will explore the
+## text classification problem of identifying complex words.
+## We have provided the following skeleton for your code,
+## with several helper functions, and all the required
+## functions you need to write.
+#############################################################
+
+from collections import defaultdict
+import gzip
+
+def sum_el(same, val, pred_list, true_list):
+    sum = 0
+    for i in range(pred_list):
+        if true_list[i] == val:
+            if (pred_list[i] == true_list[i]) == same:
+                sum += 1 
+    return sum
+
+
+#### 1. Evaluation Metrics ####
+
+## Input: y_pred, a list of length n with the predicted labels,
+## y_true, a list of length n with the true labels
+
+## Calculates the precision of the predicted labels
+def get_precision(y_pred, y_true):
+    precision = sum_el(same = True, val = 1, pred_list = y_pred, true_list = y_true) / (
+        sum_el(same = True, val = 1, pred_list = y_pred, true_list = y_true) + 
+        sum_el(same = False, val = 0, pred_list = y_pred, true_list = y_true))
+
+    return precision
+    
+## Calculates the recall of the predicted labels
+def get_recall(y_pred, y_true):
+    recall = sum_el(same = True, val = 1, pred_list = y_pred, true_list = y_true) / (
+        sum_el(same = True, val = 1, pred_list = y_pred, true_list = y_true) +
+        sum_el(same = False, val = 1, pred_list = y_pred, true_list = y_true))
+    return recall
+
+## Calculates the f-score of the predicted labels
+def get_fscore(y_pred, y_true):
+    recall = get_recall(y_pred, y_true)
+    precision = get_precision(y_pred, y_true)
+    fscore = 2 * recall * precision/(recall + precision)
+    return fscore
+
+#### 2. Complex Word Identification ####
+
+def test_predictions(y_pred, y_true):
+    print("Recall: " + get_recall(y_pred, y_true))
+    print("Precision: " + get_precision(y_pred, y_true))
+    print("Fscore: " + get_fscore(y_pred, y_true))
+
+## Loads in the words and labels of one of the datasets
+def load_file(data_file):
+    words = []
+    labels = []   
+    with open(data_file, 'rt', encoding="utf8") as f:
+        i = 0
+        for line in f:
+            if i > 0:
+                line_split = line[:-1].split("\t")
+                words.append(line_split[0])
+                labels.append(int(line_split[1]))
+            i += 1
+    return words, labels
+
+### 2.1: A very simple baseline
+
+## Labels every word complex
+def all_complex(data_file):
+    training_dic = load_file(data_file)
+    pred_dic = defaultdict()
+    for key in training_dic.keys():
+        pred_dic[key] = 1
+    
+    performance = [precision, recall, fscore]
+    return performance
+
+
+### 2.2: Word length thresholding
+
+## Finds the best length threshold by f-score, and uses this threshold to
+## classify the training and development set
+def word_length_threshold(training_file, development_file):
+    ## YOUR CODE HERE
+    training_performance = [tprecision, trecall, tfscore]
+    development_performance = [dprecision, drecall, dfscore]
+    return training_performance, development_performance
+
+### 2.3: Word frequency thresholding
+
+## Loads Google NGram counts
+def load_ngram_counts(ngram_counts_file): 
+   counts = defaultdict(int) 
+   with gzip.open(ngram_counts_file, 'rt') as f: 
+       for line in f:
+           token, count = line.strip().split('\t') 
+           if token[0].islower(): 
+               counts[token] = int(count) 
+   return counts
+
+# Finds the best frequency threshold by f-score, and uses this threshold to
+## classify the training and development set
+def word_frequency_threshold(training_file, development_file, counts):
+    ## YOUR CODE HERE
+    training_performance = [tprecision, trecall, tfscore]
+    development_performance = [dprecision, drecall, dfscore]
+    return training_performance, development_performance
+
+### 2.4: Naive Bayes
+        
+## Trains a Naive Bayes classifier using length and frequency features
+def naive_bayes(training_file, development_file, counts):
+    ## YOUR CODE HERE
+    training_performance = [tprecision, trecall, tfscore]
+    development_performance = [dprecision, drecall, dfscore]
+    return training_performance, development_performance
+
+### 2.5: Logistic Regression
+
+## Trains a Naive Bayes classifier using length and frequency features
+def logistic_regression(training_file, development_file, counts):
+    ## YOUR CODE HERE    
+    training_performance = [tprecision, trecall, tfscore]
+    development_performance = [dprecision, drecall, dfscore]
+    return training_performance, development_performance
+
+### 2.7: Build your own classifier
+
+## Trains a classifier of your choosing, predicts labels for the test dataset
+## and writes the predicted labels to the text file 'test_labels.txt',
+## with ONE LABEL PER LINE
+
+
+if __name__ == "__main__":
+    training_file = "data/complex_words_training.txt"
+    development_file = "data/complex_words_development.txt"
+    test_file = "data/complex_words_test_unlabeled.txt"
+
+    train_data = load_file(training_file)
+    
+    ngram_counts_file = "ngram_counts.txt.gz"
+    counts = load_ngram_counts(ngram_counts_file)
